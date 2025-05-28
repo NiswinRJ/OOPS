ffe85fe27a5a9efb874d1dd5b2b5ccc05bb0f7c2
Aashik Niswin RJ <aashik.robinson@relevantz.com>
2025-05-28 20:13:58 +0530

    Access Modifiers

---
+++ Access Modifiers/Access_Modifiers/Program.cs
@@ -0,0 +1,50 @@
+﻿class MakingUseOfAccessSpecifiers{
+
+    int propertyNumber;
+
+    public MakingUseOfAccessSpecifiers(){
+        propertyNumber=99902020;
+    }
+    public MakingUseOfAccessSpecifiers(int propertyNumber){
+        this.propertyNumber=propertyNumber;
+    }
+
+
+    //Accessible anywhere
+    public void PublicMethod(){
+        Console.WriteLine("This is Public Method use.....");
+    }
+
+    //Accessible only within the class
+    private void PrivateMethod(){
+        Console.WriteLine("This is a Private Method use.....");
+    }
+
+
+    //Accessible within the class and derived class only
+    protected void ProtectedMethod(){
+        Console.WriteLine("This is Protected Method use.....");
+    }
+
+
+    //Accessible within the same Assembly file
+    internal void InternalMethod(){
+        Console.WriteLine("This is a Internal Method use.....");
+    }
+
+
+    //Accessible within the same protected access-Assembly file
+    protected internal void ProtectedInternalMethod(){
+        Console.WriteLine("This is a Protected Internal Method use.....");
+    }
+
+}
+
+
+class TestAccess{
+    static void Main(string[] args){
+        MakingUseOfAccessSpecifiers myInstance=new MakingUseOfAccessSpecifiers();
+        MakingUseOfAccessSpecifiers myInstance1=new MakingUseOfAccessSpecifiers(5678);
+    }
+}
+    
---
+++ Access Modifiers/Task-Access Specifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿class House{
+    
+    public string propertyNumber;
+    public string propertyLocation;
+    public int propertyPrice;
+
+
+    public House(string propertyNumber, string propertyLocation,int propertyPrice){
+        this.propertyNumber = propertyNumber;
+        this.propertyLocation = propertyLocation;
+        this.propertyPrice = propertyPrice;
+    }
+}
+class Virudhunagar{
+    static void Main(){
+        House house = new House("24/k","Virudhunagar",5000000);
+        System.Console.WriteLine(house.propertyNumber);
+    }
+}
