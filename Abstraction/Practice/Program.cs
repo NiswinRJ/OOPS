b4476f67b0ceb07841b9583a3d5615242d202e9d
Aashik Niswin RJ <aashik.robinson@relevantz.com>
2025-05-28 20:12:45 +0530

    Abstraction

---
+++ Abstraction/Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿public abstract class Shapes{
+    public abstract void Display();
+}
+
+public class Square:Shapes{
+    public override void Display()
+    {
+        Console.WriteLine("Printing Square");
+    }
+}
+public class Rectangle:Shapes{
+    public override void Display()
+    {
+        Console.WriteLine("Printing Rectangle");
+    }
+}
+
+class Excercise{
+    static void Main(){
+        Square drawshape=new Square();
+        drawshape.Display();
+        Rectangle draw=new Rectangle();
+        draw.Display();
+        Console.ReadKey();
+    }
+}
